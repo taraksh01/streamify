71e7536422c500d4df5362cdb07c3fbaaf9bdbd6
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-04 15:01:37 +0530

    Listening to the app after the database is connected

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: ".env" });
 
-connectDB();
+connectDB()
+  .then(
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    }),
+
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`),
+    ),
+  )
+  .catch((error) => {
+    console.error(`MongoDb connection failed: ${error.message}`);
+    process.exit(1);
+  });
