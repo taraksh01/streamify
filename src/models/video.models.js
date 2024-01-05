fe291150f04ad582120212fbf0b9c90a2775b0fc
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-05 19:33:26 +0530

    Created video schema

---
+++ src/models/video.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 80,
+      index: true,
+    },
+    video: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    description: { type: String, required: true, trim: true, minLength: 2000 },
+    duration: { type: Number, required: true },
+    views: { type: Number, default: 0 },
+    tags: { type: [String], required: true },
+    isPublished: { type: Boolean, default: false },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Video = mongoose.model("Video", videoSchema);
