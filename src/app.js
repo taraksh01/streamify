4094ae4b5ee65d84600b29be80eb0ff9c1adc4d4
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-06 19:27:08 +0530

    Added cookie parser

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 
 const app = express();
@@ -12,5 +13,6 @@ app.use(
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
