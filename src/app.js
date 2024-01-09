ebfafa848db6853204d88cc0ddb74a212fde7f88
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-09 21:01:16 +0530

    Using users router at /api/v1/users

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -14,5 +15,11 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
