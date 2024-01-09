59b7d587bf7763c2f84b2b6e9e62884fc64a0fac
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-09 20:43:52 +0530

    Imported app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: ".env" });
 
 connectDB()
   .then(
-    app.on("error", (error) => {
-      console.log(error);
-      throw error;
-    }),
-
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`),
+    () => (
+      app.on("error", (error) => {
+        console.log(error);
+        throw error;
+      }),
+      app.listen(process.env.PORT, () =>
+        console.log(`Server running on port ${process.env.PORT}`),
+      )
     ),
   )
   .catch((error) => {
