f8fcb9100b3a9e6de51d08c4379a99712e0e71b0
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-10 21:04:34 +0530

    Validating user details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // steps to register a user
@@ -10,6 +11,19 @@ const registerUser = asyncHandler(async (req, res) => {
   // remove password and refresh token from response
   // check if user created successfully
   // send response back to user on frontend
+
+  const { username, fullName, email, password } = req.body;
+  const regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[gmail,protommail,outlook]+(?:\.[a-zA-Z0-9-]+)*$/g;
+  if (username.trim().length < 3) {
+    throw new ApiError(400, "Username must be at least 3 characters long");
+  } else if (!email.match(regex)) {
+    throw new ApiError(400, "Email is not valid");
+  } else if (fullName.trim().length < 3) {
+    throw new ApiError(400, "Full name must be at least 3 characters long");
+  } else if (password.trim().length < 8) {
+    throw new ApiError(400, "Password must be at least 8 characters long");
+  }
 });
 
 export { registerUser };
