466f5a48aaf2c5aea91d601ba35989e71dffe176
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-10 20:00:00 +0530

    Written steps to follow to register  a user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "ok",
-  });
+  // steps to register a user
+  // get user details from frontend
+  // validate user details
+  // check if user already exists
+  // upload images to cloudinary
+  // create user object and create entry in db
+  // remove password and refresh token from response
+  // check if user created successfully
+  // send response back to user on frontend
 });
 
 export { registerUser };
