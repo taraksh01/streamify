ea2cd142e13b83cd82888f0f1364a5295d3eeaa2
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-12 19:31:44 +0530

    Cheking modification on password filed instead of value of password

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified(this.password)) return next();
+  if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
