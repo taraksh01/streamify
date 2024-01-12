ea18312a93eac14de67751472267688290ce07b5
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-12 19:47:57 +0530

    Sending response with user details to the frontend

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -115,6 +115,27 @@ const loginUser = asyncHandler(async (req, res) => {
   const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
     user._id,
   );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(200, "User logged in successfully", {
+        user: loggedInUser,
+        accessToken,
+        refreshToken,
+      }),
+    );
 });
 
 export { registerUser, loginUser };
