e50beafa4d856e6d3a6fbd886d296a40e34679bd
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-13 11:16:53 +0530

    Added secure route for logging out user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -13,5 +18,8 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
+
+// Secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
