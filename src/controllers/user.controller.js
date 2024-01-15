398db8f17ee2b3138d8111605130d3d93249de7f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-15 18:54:35 +0530

    Created function to change user password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -203,4 +203,27 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changePassword = asyncHandler(async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user._id);
+
+  const isPasswordCorrect = await user.isPasswordCorrect(currentPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Incorrect password");
+  }
+
+  user.password = newPassword;
+  user.save({ ValidityState: true });
+
+  return res.status(200).json(new ApiResponse(200, "Password changed", {}));
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changePassword,
+};
