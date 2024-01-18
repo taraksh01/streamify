9f2c9a0fc9de495543557441e4097a8cf9387d5f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-18 20:02:39 +0530

    Created playlist model

---
+++ src/models/playlist.models.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    videos: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Video", required: true },
+    ],
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
