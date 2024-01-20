a48a153ac39cf683c4200d4c3936d71cc6236a93
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-20 19:10:42 +0530

    Created function to create tweet

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,30 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { Tweet } from "../models/Tweet";
+import { ApiError } from "../utils/ApiError";
+import { uploadOnCloudinary } from "../utils/cloudinary";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const createTweet = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+
+  if (content.trim().length === 0) {
+    throw new ApiError(400, "Tweet cannot be empty");
+  }
+
+  const imageLocalPath = req.file?.path;
+  const imageCloudinaryUrl = await uploadOnCloudinary(imageLocalPath);
+
+  const tweet = await Tweet.create({
+    content: content.trim(),
+    author: req.user._id,
+    image: imageCloudinaryUrl,
+  });
+
+  if (!tweet) {
+    throw new ApiError(500, "Something went wrong while creating tweet");
+  }
+
+  return res.status(201).json(new ApiResponse(200, "Tweet created", tweet));
+});
+
+export { createTweet };
