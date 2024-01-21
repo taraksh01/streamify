40d43eafa88fb7464b436038d29bfe4e49239f12
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-21 21:23:00 +0530

    Add path for tweet

--- src/app.js
+++ src/app.js
@@ -19,7 +19,9 @@ app.use(cookieParser());
 // routes
 
 import userRouter from "./routes/user.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 export { app };
