48035f37da7af6e3ce2355cc9f4ff0b62c5109d7
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-22 19:23:35 +0530

    Add route to get tweet with dynamic id

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, getTweet } from "../controllers/tweet.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.route("/create").post(verifyJWT, upload.single("image"), createTweet);
+router.route("/:id").get(getTweet);
 
 export default router;
