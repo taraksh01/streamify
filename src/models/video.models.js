27e67cce88ffbbf3822563b7c1a60cac8d9e7304
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-24 14:58:57 +0530

    Remove tags from video model and replace minLength with maxLength for description

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -12,10 +12,9 @@ const videoSchema = new mongoose.Schema(
     },
     video: { type: String, required: true },
     thumbnail: { type: String, required: true },
-    description: { type: String, required: true, trim: true, minLength: 2000 },
+    description: { type: String, required: true, trim: true, maxLength: 2000 },
     duration: { type: Number, required: true },
     views: { type: Number, default: 0 },
-    tags: { type: [String], required: true },
     isPublished: { type: Boolean, default: false },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
