e100267f1c7af0dbe54d935a8cddb56909faf290
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-25 14:26:10 +0530

    Add route  to update video thumnail

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -4,6 +4,7 @@ import {
   getAllVideos,
   getVideo,
   publishVideo,
+  updateThumbnail,
   updateVideoDetails,
 } from "../controllers/video.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -21,5 +22,8 @@ router.route("/").post(
 router.route("/:username").get(getAllVideos);
 router.route("/watch/:id").get(getVideo);
 router.route("/update/video-details/:id").patch(updateVideoDetails);
+router
+  .route("/update/thumbnail/:id")
+  .patch(upload.single("thumbnail"), updateThumbnail);
 
 export default router;
