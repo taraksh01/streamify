e544001153723afdcf94f77efdcb3943cc6c78c3
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-26 18:18:37 +0530

    Add route to post comment on tweet

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createVideoComment } from "../controllers/comment.controller.js";
+import {
+  createTweetComment,
+  createVideoComment,
+} from "../controllers/comment.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/video/:id").post(createVideoComment);
+router.route("/tweet/:id").post(createTweetComment);
 
 export default router;
