87dcb9bbe2a75e92c2ac582f72a808a389cafca9
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-26 18:18:15 +0530

    Write controller to create a comment on tweet

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Video } from "../models/video.models.js";
+import { Tweet } from "../models/tweet.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Comment } from "../models/comment.models.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -26,4 +27,26 @@ const createVideoComment = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "Comment created successfully", comment));
 });
 
-export { createVideoComment };
+const createTweetComment = asyncHandler(async (req, res) => {
+  const tweet = await Tweet.findById(req.params.id);
+
+  if (!tweet) throw new ApiError(404, "Tweet not found");
+
+  const { content } = req.body;
+
+  if (!content) throw new ApiError(400, "Comment is required");
+
+  const comment = await Comment.create({
+    content,
+    author: req.user._id,
+    tweet: tweet._id,
+  });
+
+  if (!comment) throw new ApiError(500, "Server error while creating comment");
+
+  res
+    .status(201)
+    .json(new ApiResponse(200, "Comment created successfully", comment));
+});
+
+export { createVideoComment, createTweetComment };
