31b5711bc0db113ebfe43c7347a12fce4b34abfb
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-26 19:48:37 +0530

    Add route to delete a comment using its id

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -3,6 +3,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   createTweetComment,
   createVideoComment,
+  deleteComment,
   getAllCommentsOnTweet,
   getAllCommentsOnVideo,
   getComment,
@@ -15,6 +16,6 @@ router.use(verifyJWT);
 
 router.route("/video/:id").get(getAllCommentsOnVideo).post(createVideoComment);
 router.route("/tweet/:id").get(getAllCommentsOnTweet).post(createTweetComment);
-router.route("/:id").get(getComment).patch(updateComment);
+router.route("/:id").get(getComment).patch(updateComment).delete(deleteComment);
 
 export default router;
