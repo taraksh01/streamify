5c2ea69be4b23185178f9143fbfed6eaef33dc91
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-26 18:55:12 +0530

    Add route to fetch comments on video

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -3,13 +3,14 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   createTweetComment,
   createVideoComment,
+  getAllCommentsOnVideo,
 } from "../controllers/comment.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/video/:id").post(createVideoComment);
+router.route("/video/:id").get(getAllCommentsOnVideo).post(createVideoComment);
 router.route("/tweet/:id").post(createTweetComment);
 
 export default router;
