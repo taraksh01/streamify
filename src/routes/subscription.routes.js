5b686ce834419af345340241693d0d5a2b58e253
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-28 09:32:51 +0530

    Add route to get subscriber list

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getSubscribedChannelList } from "../controllers/subscription.controller.js";
+import {
+  getSubscribedChannelList,
+  getSubscribersList,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/").get(getSubscribedChannelList);
+router.route("/:id").get(getSubscribersList);
 
 export default router;
