a67dc0f9671cffcf37ccde6dcc691aa37c51f802
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-28 09:20:26 +0530

    Add route to fecth subscription details

--- src/app.js
+++ src/app.js
@@ -22,10 +22,12 @@ import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
 
 export { app };
