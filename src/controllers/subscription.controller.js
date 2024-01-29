143dfe7f8f664279460df71ca0bc8c659704746f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-29 20:14:50 +0530

    Update controller to fetch subscribed channels list

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,36 +1,49 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.models.js";
+import { Subscription } from "../models/subscription.models.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import mongoose from "mongoose";
 
 const getSubscribedChannelList = asyncHandler(async (req, res) => {
-  const channelList = await User.aggregate([
-    { $match: { subscriber: req.user._id } },
-    {
-      $lookup: {
-        from: "users",
-        localField: "subscriber",
-        foreignField: "_id",
-        as: "subscriber",
+  try {
+    const channelList = await Subscription.aggregate([
+      {
+        $match: { subscriber: new mongoose.Types.ObjectId(req.user.id) },
       },
-    },
-    {
-      $project: {
-        fullName: { $arrayElemAt: ["$subscriber.fullName", 0] },
-        username: { $arrayElemAt: ["$subscriber.username", 0] },
-        profilePicture: { $arrayElemAt: ["$subscriber.avatar", 0] },
+      {
+        $lookup: {
+          from: "users",
+          localField: "subscribedTo",
+          foreignField: "_id",
+          as: "channel",
+          pipeline: [{ $project: { fullName: 1, username: 1, avatar: 1 } }],
+        },
       },
-    },
-  ]);
+      {
+        $addFields: {
+          channel: { $first: "$channel" },
+        },
+      },
+      {
+        $project: {
+          channel: 1,
+        },
+      },
+    ]);
 
-  res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        "Subscribed channels list fetched successfully",
-        channelList,
-      ),
-    );
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          "Subscribed channels list fetched successfully",
+          channelList,
+        ),
+      );
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
 });
 
 const getSubscribersList = asyncHandler(async (req, res) => {
