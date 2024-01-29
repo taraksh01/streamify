359502e8ec41929311b572dad01a301d7a50b24f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-29 20:22:40 +0530

    Add route to toggle subscription

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -3,12 +3,13 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   getSubscribedChannelList,
   getSubscribersList,
+  toggleSubscribe,
 } from "../controllers/subscription.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/").get(getSubscribedChannelList);
+router.route("/").get(getSubscribedChannelList).patch(toggleSubscribe);
 router.route("/:id").get(getSubscribersList);
 
 export default router;
