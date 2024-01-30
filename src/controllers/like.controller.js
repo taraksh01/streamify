a0a919e32b492f4db7f56c7eb8fbf21317b3192d
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-30 18:50:41 +0530

    Write controller to toggle like on comment by using comment id

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,5 +1,6 @@
 import { Video } from "../models/video.models.js";
 import { Like } from "../models/like.models.js";
+import { Comment } from "../models/comment.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -24,4 +25,27 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "Like toggled successfully"));
 });
 
-export { toggleVideoLike };
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) throw new ApiError(400, "Comment id is missing");
+
+  const comment = await Comment.findOne({ _id: commentId });
+  if (!comment) throw new ApiError(404, "Comment not found");
+
+  const isLiked = await Like.findOne({
+    comment: commentId,
+    likedBy: req.user._id,
+  });
+
+  if (isLiked) {
+    await Like.deleteOne({ comment: commentId, likedBy: req.user._id });
+  } else {
+    await Like.create({ comment: commentId, likedBy: req.user._id });
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Like toggled successfully"));
+});
+
+export { toggleVideoLike, toggleCommentLike };
