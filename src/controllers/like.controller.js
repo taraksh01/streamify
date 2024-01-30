544d82f759132bbe67108231f718c6bfebc6362c
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-30 18:56:57 +0530

    Write controller to toggle like on tweet

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,6 +1,7 @@
 import { Video } from "../models/video.models.js";
 import { Like } from "../models/like.models.js";
 import { Comment } from "../models/comment.models.js";
+import { Tweet } from "../models/tweet.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -48,4 +49,24 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "Like toggled successfully"));
 });
 
-export { toggleVideoLike, toggleCommentLike };
+const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  if (!tweetId) throw new ApiError(400, "Tweet id is missing");
+
+  const tweet = await Tweet.findOne({ _id: tweetId });
+  if (!tweet) throw new ApiError(404, "Tweet not found");
+
+  const isLiked = await Like.findOne({ tweet: tweetId, likedBy: req.user._id });
+
+  if (isLiked) {
+    await Like.deleteOne({ tweet: tweetId, likedBy: req.user._id });
+  } else {
+    await Like.create({ tweet: tweetId, likedBy: req.user._id });
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Like toggled successfully"));
+});
+
+export { toggleVideoLike, toggleCommentLike, toggleTweetLike };
