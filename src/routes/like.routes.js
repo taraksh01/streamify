2b1d2de3281e6f71f984397c8b84cbe24d8c438f
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-30 19:08:57 +0530

    Add route to get all liked videos

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
+  getLikedVideos,
   toggleCommentLike,
   toggleTweetLike,
   toggleVideoLike,
@@ -13,5 +14,7 @@ router.use(verifyJWT);
 router.route("/video/:videoId").patch(toggleVideoLike);
 router.route("/comment/:commentId").patch(toggleCommentLike);
 router.route("/tweet/:tweetId").patch(toggleTweetLike);
+
+router.route("/videos").get(getLikedVideos);
 
 export default router;
