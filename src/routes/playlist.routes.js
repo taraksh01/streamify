95824dd2edfdd4d9cbc29a224d1f2c4d955be8f6
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-31 20:04:15 +0530

    Add route to update playlist details

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { cteatePlaylist } from "../controllers/playlist.controller.js";
+import {
+  cteatePlaylist,
+  updatePlaylist,
+} from "../controllers/playlist.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/").post(cteatePlaylist);
+router.route("/:playlistId").patch(updatePlaylist);
 
 export default router;
