2e5d698265edf5fe7788fc759073f47a35ea18ba
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-01-31 20:08:15 +0530

    Add route to fetch playlist using its id

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   cteatePlaylist,
+  getPlaylist,
   updatePlaylist,
 } from "../controllers/playlist.controller.js";
 
@@ -9,6 +10,6 @@ const router = Router();
 router.use(verifyJWT);
 
 router.route("/").post(cteatePlaylist);
-router.route("/:playlistId").patch(updatePlaylist);
+router.route("/:playlistId").patch(updatePlaylist).get(getPlaylist);
 
 export default router;
